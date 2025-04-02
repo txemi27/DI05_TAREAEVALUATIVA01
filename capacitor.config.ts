27f8e83d72c5afedba1372ae76667de2bae1e827
Jose Maria Palomino <jmpalomino@airlan.es>
2025-04-02 12:20:08 +0200

    pruebaBrach

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'tarea_evaluativa01',
+  appName: 'DI05tarea_evaluativa01',
   webDir: 'www'
 };
 
